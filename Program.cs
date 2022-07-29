8f07ac8855691d8425ce1129b85fda0228c95120
Lena <len.dal@yandex.ru>
2022-07-29 22:31:46 +0300

    input integer size array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿// 1 определяю сколько строк в массиве хочет ввести пользователь, задаю размер массива 
+static int InputInteger(string size)
+{
+    Console.Write(size+":");
+    if (int.TryParse(Console.ReadLine(), out int value))
+    return value;
+    return InputInteger(size);
+}
